457b9813fbe1ea95c54ddb43ad0e7a04959e4aa8
Daniel Brodsky <dnbrdsky@gmail.com>
2020-10-13 04:28:22 -0700

    added register parsing

--- src/blif.rs
+++ src/blif.rs
@@ -15,6 +15,7 @@ use nom::{
     combinator::rest,
     named_args,
     preceded,
+    character::complete::multispace1
 };
 
 use std::collections::HashMap;
@@ -47,7 +48,46 @@ impl LUT {
     }
 }
 
+#[derive(Debug, Eq, PartialEq)]
 pub struct Register {
+    input: Var,
+    output: Var,
+    signal: String,
+    control: Var,
+    init: u8 // 0 = lo, 1 = hi, 2 = don't care, 3 = unknown
+}
+
+impl Register {
+
+    pub fn new(input: &str,
+               output: &str,
+               clock: Option<(&str, &str)>,
+               init: Option<char>) -> Register {
+
+        // TODO: default global clock for latch
+        let mut signal = "re";
+        let mut control = "NIL";
+        let mut start: u8 = 3;
+        match clock {
+            Some(clk) => {
+                signal = clk.0;
+                control = clk.1;
+            }
+            None => {}
+        };
+        match init {
+            Some(i) => { start = i.to_digit(10).unwrap() as u8; }
+            None => {}
+        };
+
+        Register{
+            input: Var::new(input),
+            output: Var::new(output),
+            signal: signal.to_string(),
+            control: Var::new(control),
+            init: start
+        }
+    }
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -57,9 +97,10 @@ pub struct Var  {
 
 impl Var {
 
-    pub fn new(name: String) -> Var {
-        Var{name}
+    pub fn new<S>(name: S) -> Var where S: Into<String> {
+        Var{name: name.into()}
     }
+
 }
 
 pub enum Element {
@@ -141,6 +182,50 @@ fn test_get_lut() {
     let mut lut = LUT::new(vec!("out0","out1","out2"), "return0", vec!("011 1", "100 1"));
     assert_eq!(get_lut(".names out0 out1 out2 return0\n011 1\n100 1\n.names"), Ok((".names", lut)));
 }
+
+named!(
+    get_clock<&str, (&str, &str)>,
+    do_parse!(
+        tag!(" ") >>
+        signal: alt!(tag!("fe") | tag!("re") | tag!("ah") | tag!("al") | tag!("as")) >>
+        tag!(" ") >>
+        control: is_not!(" \n") >>
+        ((signal, control))
+    )
+);
+
+
+named!(
+    get_reg<&str, Register>,
+    do_parse!(
+        tag!(".latch ") >>
+        input: is_not!(" \n") >>
+        tag!(" ") >>
+        output: is_not!(" \n") >>
+        clock: opt!(get_clock) >>
+        opt!(complete!(tag!(" "))) >>
+        init: opt!(one_of!("0123")) >>
+        newline >>
+        (Register::new(input, output, clock, init))
+    )
+);
+
+#[test]
+fn test_get_reg() {
+    let mut reg = Register::new("$0out[8:0][8]", "out[8]", Some(("re", "clock")), Some('2'));
+    assert_eq!(get_reg(".latch $0out[8:0][8] out[8] re clock 2\n"), Ok(("", reg)));
+
+    let mut reg = Register::new("$0out[8:0][8]", "out[8]", Some(("re", "clock")), None);
+    assert_eq!(get_reg(".latch $0out[8:0][8] out[8] re clock\n"), Ok(("", reg)));
+
+    let mut reg = Register::new("$0out[8:0][8]", "out[8]", None, None);
+    assert_eq!(get_reg(".latch $0out[8:0][8] out[8]\n"), Ok(("", reg)));
+
+    let mut reg = Register::new("$0out[8:0][8]", "out[8]", None, Some('2'));
+    assert_eq!(get_reg(".latch $0out[8:0][8] out[8] 2\n"), Ok(("", reg)));
+}
+
+
 
 
 #[test]
